3d914ca6bda51eaca1f8125e9171e478c6c9c69f
ViniciusOkaeda <viniciusokaeda@gmail.com>
2023-06-29 17:43:55 -0300

    channels page quase finalizada

--- js/moTV/channelCalls/subscribedChannels.js
+++ js/moTV/channelCalls/subscribedChannels.js
@@ -1,31 +1,49 @@
+const auth = localStorage.getItem("authorization");
+const profile = localStorage.getItem("profileid");
+const language = 'pt';
+const devicesType = 'webos';
+function adicionaZero(numero){
+    if (numero <= 9) 
+        return "0" + numero;
+    else
+        return numero; 
+}
+const timeGet = new Date();
+const hours = timeGet.getHours();
+const minutes = timeGet.getMinutes();
+const seconds = timeGet.getMinutes();
+const currentYear = timeGet.getFullYear();
+const today = timeGet.getDate();
+const currentMonth = timeGet.getMonth() + 1; 
+const timezone = timeGet.getTime();
+
+let getMonthFormated = (adicionaZero(timeGet.getMonth()+1).toString() );
+let getDayFormated = (adicionaZero(timeGet.getDate().toString()) );
+//console.log("mes formatado", getMonthFormated);
+//console.log("dia formatado", getDayFormated);
+
+const fullDate = currentYear + "-" + getMonthFormated + "-" + getDayFormated + "T" + hours + ":" + minutes + ":" + seconds
+const fullDate2 = currentYear + "-" + getMonthFormated + "-" + getDayFormated + "T" + hours 
+const fullDate3 = currentYear + "-" + getMonthFormated + "-" + getDayFormated
+const stringDate = fullDate.toString();
+
+//console.log ("o str date", stringDate)
+
+
 async function getSubscribedChannels() {
-    const auth = localStorage.getItem("authorization");
-    const profile = localStorage.getItem("profileid");
-    const language = 'pt';
-    const devicesType = 'webos';
 
     var loaded = document.getElementById("loadingContent");
     var load1 = false;
-    var load2 = false;
-    var load3 = false;
-    var load4 = false;
+
     showLoading();
+    if(localStorage.getItem("catId") == null) {
+        localStorage.setItem("catId", "null")
+    }
 
-    const test = Date.now()
-    const timeGet = new Date();
-    const hours = timeGet.getHours();
-    const minutes = timeGet.getMinutes();
-    const seconds = timeGet.getMinutes();
-    const currentYear = timeGet.getFullYear();
-    const today = timeGet.getDate();
-    const currentMonth = timeGet.getMonth() + 1; 
-    const timezone = timeGet.getTime();
-    console.log("o teste", timeGet.getTimezoneOffset());
-    const fullDate = currentYear + "-" + currentMonth + "-" + today + "T" + hours + ":" + minutes + ":" + seconds
-    const stringDate = fullDate.toString();
-    console.log ("o str date", stringDate)
 
-    const getSubscribedAndLockedChannels = await axios.post('https://hospitality.youcast.tv.br/getSubscribedAndLockedChannels', {
+
+    async function getSubscribedAndLockedChannels() {
+        await axios.post('https://hospitality.youcast.tv.br/getSubscribedAndLockedChannels', {
         authorization: 'Bearer ' + auth,
         includeData: true,
         profileId: profile,
@@ -33,32 +51,70 @@ async function getSubscribedChannels() {
         devicesType: devicesType,
     }).then(function (response) {
         if(response.data.status == 1){
-            load1 = true
-            console.log("o getSubscribedAndLockedChannels", response.data.response);
-            //showEventInitial(response.data.response);
-        }
-    }).catch(function (response) {
-        console.log("o response de erro", response)
-    })
+            var allChannels = response.data.response;
 
-    const getUpdateEpgEventsV2 = await axios.post('https://hospitality.youcast.tv.br/getEpgUpdatedEventsV2', {
-        authorization: 'Bearer ' + auth,
-        includeData: true,
-        profileId: profile,
-        language: language,
-        devicesType: devicesType,
-        timestamp: 0,
-        from: stringDate,
-        to: stringDate
-    }).then(function (response) {
-        if(response.data.status == 1){
-            load2 = true
-            console.log("o getUpdateEpgEventsV2", response.data.response);
-            //showEventInitial(response.data.response);
+            async function getUpdateEpgEventsV2() {
+                await axios.post('https://hospitality.youcast.tv.br/getEpgUpdatedEventsV2', {
+                authorization: 'Bearer ' + auth,
+                includeData: true,
+                profileId: profile,
+                language: language,
+                devicesType: devicesType,
+                timestamp: 0,
+                from: stringDate,
+                to: stringDate
+            }).then(function (response) {
+                if(response.data.status == 1){
+                    var updateEpgEvents = response.data.response
+                    console.log("o getEpgInfo", updateEpgEvents)
+
+                    async function getFavoriteChannels() {
+                        await axios.post('https://hospitality.youcast.tv.br/getFavoriteChannels', {
+                        authorization: 'Bearer ' + auth,
+                        includeData: true,
+                        profileId: profile,
+                        language: language,
+                        devicesType: devicesType,
+                        timestamp: 0
+                    }).then(function (response) {
+                        if(response.data.status == 1){
+                            load1 = true;
+                            if(load1 == true ) {
+                                loaded.style.display = "none";
+                            }
+                            //showFocusedEvent(updateEpgEvents);
+                            //console.log("o getFavoriteChannels", response.data.response);
+                            filterAllChannels(allChannels, updateEpgEvents, response.data.response);
+        
+                        }
+                    }).catch(function (response) {
+                        console.log("o response de erro", response)
+                    })
+                    } 
+                    getFavoriteChannels();
+        
+                    //console.log("o getUpdateEpgEventsV2", response.data.response);
+                    //showEventInitial(response.data.response);
+                    //showFocusedEvent(response.data.response);
+                }
+            }).catch(function (response) {
+                console.log("o response de erro", response)
+            })
+            }
+            getUpdateEpgEventsV2() 
+
+            //console.log("o getSubscribedAndLockedChannels", allChannels)
+
         }
     }).catch(function (response) {
         console.log("o response de erro", response)
     })
+    }
+    getSubscribedAndLockedChannels(); 
+
+
+
+
 
     const getChannelCategories = await axios.post('https://hospitality.youcast.tv.br/getChannelCategories', {
         authorization: 'Bearer ' + auth,
@@ -69,7 +125,7 @@ async function getSubscribedChannels() {
         timestamp: 0
     }).then(function (response) {
         if(response.data.status == 1){
-            load3 = true;
+            
             showChannelCategories(response.data.response)
             console.log("o getChannelCategories", response.data.response);
             //showEventInitial(response.data.response);
@@ -77,27 +133,6 @@ async function getSubscribedChannels() {
     }).catch(function (response) {
         console.log("o response de erro", response)
     })
-
-    const getFavoriteChannels = await axios.post('https://hospitality.youcast.tv.br/getFavoriteChannels', {
-        authorization: 'Bearer ' + auth,
-        includeData: true,
-        profileId: profile,
-        language: language,
-        devicesType: devicesType,
-        timestamp: 0
-    }).then(function (response) {
-        if(response.data.status == 1){
-            load4 = true;
-            console.log("o getFavoriteChannels", response.data.response);
-            //showEventInitial(response.data.response);
-        }
-    }).catch(function (response) {
-        console.log("o response de erro", response)
-    })
-
-if(load1 == true && load2 == true && load3 == true && load4 == true) {
-    loaded.style.display = "none";
-}
 
 }
 
@@ -122,30 +157,101 @@ function showLoading() {
     `
 }
 
-
 function showFocusedChannel() {
 
-    document.getElementById("focusedChannel").innerHTML = 
-    `
-    <div class="focusedContainer">
-        <div class="focusedTextInfo"></div>
-        <div class="focusedImageInfo"></div>
-    </div>
-    `
+    if(event == undefined) {
+        document.getElementById("focusedChannel").innerHTML = 
+        `
+        <div class="focusedContainer">
+            <div class="focusedTextInfo">
+                <div class="focusedLogoContent">
+                    <div class="focusedLogoImage">
+                        <div class="focusedShowLogo" style="background-image: url(${"../../images/logo-accorinvest-branco.png"})"></div>
+                    </div>
+
+                    <div class="focusedLogoName"></div>
+                </div>
+
+                <div></div>
+            </div>
+
+            <div class="focusedImageInfo">
+            
+            </div>
+        </div>
+        `
+    }else if(event.target.dataset != undefined){
+        const eventDetails = event.target.dataset
+        console.log("tem epg2", event.target.dataset)
+        localStorage.setItem("selectedChannel", event.target.dataset.channels_id)
+        //console.log("o epgevent", eventNow)
+        //console.log("evento seria: ", event.target.dataset)
+        document.getElementById("focusedChannel").innerHTML = 
+        `
+        <div class="focusedContainer">
+            <div class="focusedTextInfo">
+                <div class="focusedLogoContent">
+                    <div class="focusedLogoImage">
+                        <div class="focusedShowLogo" style="background-image: url(${eventDetails.channels_logo})"></div>
+                    </div>
+
+                    <div class="focusedLogoName"><h2>${eventDetails.channels_name}</h2></div>
+                </div>
+
+                <div class="focusedDetailsContainer">
+                    <div class="focusedShowGenresAndDuration">
+                        <h6>Gênero: ${eventDetails.channels_genre}</h6>
+                        <h6>|</h6>
+                        ${eventDetails.channels_season_and_ep !== "null" ? `<h6>${eventDetails.channels_season_and_ep}</h6>` : ''}
+                        ${eventDetails.channels_season_and_ep !== "null" ? `<h6>|</h6>` : ''}
+                        <h6>Início em: ${eventDetails.channels_start}</h6>
+                    </div>
+
+                    <div class="focusedShowDescription">
+                        <h3>Hoje: ${eventDetails.channels_title}</h3>
+                        <h4>${eventDetails.channels_description}</h4>
+                    </div>
+
+                    <div class="focusedShowGenresAndDuration">
+                    <h6>Duração: ${parseInt(eventDetails.channels_duration) / 60}m</h6>
+                    </div>
+                </div>
+            </div>
+
+            <div class="focusedImageInfo"
+
+            style="
+            background-image: linear-gradient(to bottom, rgba(0, 0, 0, 0.3) 0%, rgba(29, 32, 33, 1) 100%), url(${eventDetails.channels_image_widescreen !== "null" ? eventDetails.channels_image_widescreen : eventDetails.channels_image})
+            "
+            >
+                <div class="shadowLeft"></div>
+            
+            </div>
+        </div>
+        `
+
+    }
+
+
 }
 
+function showFocusedEvent(updateEpgEvents) {
+console.log("funcionou", updateEpgEvents);
+}
+
+
+
 function showChannelCategories(channelCategories) {
-    console.log("o channel", channelCategories)
     document.getElementById("channelCategories").innerHTML = 
     `
     <div class="channelCategoriesContent">
-    <button data-category-name="${null}" onclick="selectCategory()" class="channelButton">Todos</button>
-    <button data-category-name="Favoritos" onclick="selectCategory()" class="channelButton">Favoritos</button>
+    <button data-category-id="null" onclick="selectCategory()" class="channelButton">Todos</button>
+    <button data-category-id="4294967294" onclick="selectCategory()" class="channelButton">Favoritos</button>
     <span class="Bar">|</span>
     ${channelCategories.filter(e => e.channels_categories_name != "Favoritos").map((e, idx) => {
 
         return(`
-        <button data-category-name="${e.channels_categories_name}" onclick="selectCategory()" class="channelButton">${e.channels_categories_name}</button>
+        <button data-category-id="${e.channels_categories_id}" onclick="selectCategory()" class="channelButton">${e.channels_categories_name}</button>
         
         `);
     })}
@@ -154,29 +260,83 @@ function showChannelCategories(channelCategories) {
     
     `
 }
+
 function selectCategory() {
-    if(event.target.dataset.categoryName == "Todos") {
-        localStorage.removeItem("catName");
-    }else if(event.target.dataset.categoryName != "Todos") {
-        localStorage.setItem("catName", event.target.dataset.categoryName)
+    if(event.target.dataset.categoryId == null) {
+        localStorage.setItem("catId", "null");
+    }else if(event.target.dataset.categoryId != null) {
+        localStorage.setItem("catId", event.target.dataset.categoryId)
+        getSubscribedChannels();
     }
 
 }
 
-function showAllChannels() {
-const category = localStorage.getItem("catName");
-console.log("o sei la", category)
-switch(category){
+function filterAllChannels(subscribedChannels, epgEvents, favoriteChannels) {
+const category = localStorage.getItem("catId");
+    if(category == "null"){
+        showChannels(subscribedChannels, epgEvents);
 
-    case null: 
-        console.log("deu nulo ou Todos")
-        break;
-    case "null": 
-        console.log("deu nulo ou Todos")
-        break;
-    case "null": 
-        console.log("deu nulo ou Todos")
-        break;
+    }else if (category != "null" && category != "4294967294"){
+        //console.log("o teste", subscribedChannels.filter(e => e.channels_categories.filter(e => e == 5) == 5 ) )
+        const filteredChannels = subscribedChannels.filter(e => e.channels_categories.filter(e => e == parseInt(category)) == parseInt(category) );
+       // console.log("o resultado", filteredChannels);
+        showChannels(filteredChannels, epgEvents)
+
+    }else if (category == "4294967294") {
+        const filteredFavoriteChannels = subscribedChannels.filter(id => favoriteChannels.map(e => e).includes(id.channels_id))
+        //console.log("os canais favoritos são", filteredFavoriteChannels)
+        showChannels(filteredFavoriteChannels, epgEvents);
+    }
 }
+
+async function showChannels(channels, epgEvents) {
+
+//console.log("os canais são", channels)
+const filteredChannels = channels;
+//console.log("o testes", testes.map(e => e))
+
+//console.log("o que tem no testes", filteredChannels)
+
+document.getElementById("allChannels").innerHTML = 
+`
+<div class="allChannelsContainer">
+
+    <div class="allChannelsContent">
+        ${filteredChannels.map((item, idx) => {
+
+            const obj = epgEvents.filter(e => e.start.includes(fullDate2) ? e.start.includes(fullDate2) : e.start.includes(fullDate3));
+
+            return(`
+            <div class="channelContainer">
+                <button id="button" class="channelButtonShow"
+                onclick="showFocusedChannel()"
+                data-channels_id="${item.channels_id}"
+                data-channels_name="${item.channels_name}"
+                data-channels_logo="${item.channels_logo}"
+
+                data-channels_description="${obj.filter(e => e.channels_id == item.channels_id )[0].description}"
+                data-channels_title="${obj.filter(e => e.channels_id == item.channels_id )[0].title}"
+                data-channels_subtitle="${obj.filter(e => e.channels_id == item.channels_id )[0].subtitle}"
+                data-channels_start="${obj.filter(e => e.channels_id == item.channels_id )[0].start}"
+                data-channels_end="${obj.filter(e => e.channels_id == item.channels_id )[0].end}"
+                data-channels_image="${obj.filter(e => e.channels_id == item.channels_id )[0].image}"
+                data-channels_image_widescreen="${obj.filter(e => e.channels_id == item.channels_id )[0].image_widescreen}"
+                data-channels_genre="${obj.filter(e => e.channels_id == item.channels_id )[0].genres}"
+                data-channels_duration="${obj.filter(e => e.channels_id == item.channels_id )[0].duration}"
+                data-channels_season_and_ep="${obj.filter(e => e.channels_id == item.channels_id )[0].episode}"
+                >
+                    <div class="channelImageShow" style="background-image: url(${item.channels_logo})"></div>
+                </button>
+            </div>
+            `)
+        })}
+    </div>
+
+
+</div>
+`
+
+
 }
-showAllChannels();
+
+
